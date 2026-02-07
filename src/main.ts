0034d79a9047e58800a52ce20d8aa7c8b764d8e7
Ольга Захарова <kotfimn@mail.ru>
2026-02-07 15:23:58 +0700

    fix api route

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // или ваш Vue URL
+    origin: 'https://referhus.github.io/', // или ваш Vue URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
